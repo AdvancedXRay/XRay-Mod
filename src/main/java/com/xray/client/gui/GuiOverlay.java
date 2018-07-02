92320b8b64884bed0cedcd47e6e86cbf9e03696a
Michael Hillcox <hello.michaelhillcox@gmail.com>
2018-07-02 18:10:09 +0100

    Finished overlay for #36

--- src/main/java/com/xray/client/gui/GuiOverlay.java
+++ src/main/java/com/xray/client/gui/GuiOverlay.java
@@ -3,15 +3,19 @@ package com.xray.client.gui;
 import com.xray.client.xray.XrayController;
 import com.xray.common.XRay;
 import com.xray.common.reference.Reference;
+import java.awt.Color;
 import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.FontRenderer;
 import net.minecraft.client.gui.Gui;
 import net.minecraft.client.renderer.GlStateManager;
+import net.minecraft.client.resources.I18n;
 import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.client.event.RenderGameOverlayEvent;
 import net.minecraftforge.fml.common.eventhandler.EventPriority;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
+import org.lwjgl.opengl.GL11;
 
 public class GuiOverlay extends Gui {
 
@@ -27,10 +31,21 @@ public class GuiOverlay extends Gui {
 
         GlStateManager.enableAlpha();
         GlStateManager.enableBlend();
+        GlStateManager.color(0, 255, 0, 30);
         mc.renderEngine.bindTexture(new ResourceLocation(Reference.PREFIX_GUI + "circle.png"));
-        drawModalRectWithCustomSizedTexture(5, 5, 0f, 0f, 10, 10, 10, 10);
+        drawModalRectWithCustomSizedTexture(5, 5, 0f, 0f, 5, 5, 5, 5);
         GlStateManager.disableAlpha();
         GlStateManager.disableBlend();
+
+        GlStateManager.pushMatrix();
+        GlStateManager.enableBlend();
+        GlStateManager.blendFunc(GL11.GL_SRC_ALPHA, GL11.GL_ONE_MINUS_SRC_ALPHA);
+        FontRenderer fr = this.mc.fontRenderer;
+        int color = Color.getHSBColor(0f, 0f, 1f).getRGB();
+        fr.drawStringWithShadow(I18n.format("xray.overlay"), 15, 4, color + (30 << 24));
+        GlStateManager.disableBlend();
+        GlStateManager.popMatrix();
+
     }
 
 }
