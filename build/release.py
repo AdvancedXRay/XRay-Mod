19c65940ede84b90927f447140b2ae49fae0134b
Michael Hillcox <MichaelHillcox@users.noreply.github.com>
2018-08-16 09:42:21 +0100

    Create release.py

---
+++ build/release.py
@@ -0,0 +1,10 @@
+# This should:
+#   Open and use the release.md file
+#   Push to Curse api & Github's Release api
+#   Display status of release and allow retry of single deploy lane
+#   Allow title input
+#   Allow version number replacement
+
+#               MCVERSION-VERSION-RELEASENUMBER
+#               mc1.12.2-1.4.5-r1
+versionSchema = "mc%s-%s-r%d"
