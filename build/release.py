aad197782d798ed4371b01463c6621b4cc7ee771
Michael Hillcox <MichaelHillcox@users.noreply.github.com>
2018-08-16 09:43:10 +0100

    Update release.py

--- build/release.py
+++ build/release.py
@@ -4,6 +4,7 @@
 #   Display status of release and allow retry of single deploy lane
 #   Allow title input
 #   Allow version number replacement
+#   Edit the version.json in master to reflect the release
 
 #               MCVERSION-VERSION-RELEASENUMBER
 #               mc1.12.2-1.4.5-r1
