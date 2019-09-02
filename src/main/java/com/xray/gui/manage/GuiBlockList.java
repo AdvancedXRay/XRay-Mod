f081afea1c2c82956c93d47f64f5651d34cfa04f
Michael Hillcox <michaelhillcox@gmail.com>
2019-09-02 18:51:03 +0100

    Almost completely working scrolling implementation

--- src/main/java/com/xray/gui/manage/GuiBlockList.java
+++ src/main/java/com/xray/gui/manage/GuiBlockList.java
@@ -1,23 +1,26 @@
 package com.xray.gui.manage;
 
 import com.xray.XRay;
-import com.xray.gui.utils.AbstractScrollingList;
 import com.xray.gui.utils.GuiBase;
+import com.xray.gui.utils.ScrollingList;
 import com.xray.reference.block.BlockItem;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.FontRenderer;
 import net.minecraft.client.gui.widget.TextFieldWidget;
 import net.minecraft.client.gui.widget.button.Button;
+import net.minecraft.client.gui.widget.list.AbstractList;
 import net.minecraft.client.renderer.RenderHelper;
 import net.minecraft.client.resources.I18n;
 import net.minecraft.util.ResourceLocation;
 
+import javax.annotation.Nullable;
 import java.awt.*;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class GuiBlockList extends GuiBase {
-    private AbstractScrollingList blockList;
+    private ScrollingBlockList blockList;
     private ArrayList<BlockItem> blocks;
     private TextFieldWidget search;
     private String lastSearched = "";
@@ -28,76 +31,24 @@ public class GuiBlockList extends GuiBase {
         this.blocks = XRay.gameBlockStore.getStore();
     }
 
-    void selectBlock(int index)
-    {
-        if (index == this.selected)
-            return;
-
-        this.selected = index;
-        getMinecraft().player.closeScreen();
-        getMinecraft().displayGuiScreen( new GuiAddBlock( blocks.get( this.selected ), null ) );
-    }
-
     @Override
     public void init() {
-        this.blockList = new ScrollableList(getMinecraft(), 202, 185, height / 2 - 105, (height / 2 - 105) + 185, width / 2 - 100, 30, this.blocks);
+        this.blockList = new ScrollingBlockList((width / 2) + 1, height / 2 - 12, 202, 185, this.blocks);
         this.children.add(this.blockList);
 
         search = new TextFieldWidget(getFontRender(), width / 2 - 100, height / 2 + 85, 140, 18, "");
         search.changeFocus(true);
         search.setCanLoseFocus(true);
 
-        addButton( new Button( width / 2 +43, height / 2 + 84, 60, 20, I18n.format("xray.single.cancel"), b -> this.onClose()) );
+        addButton(new Button(width / 2 + 43, height / 2 + 84, 60, 20, I18n.format("xray.single.cancel"), b -> this.onClose()));
     }
-
 
     @Override
     public boolean charTyped(char keyTyped, int __unknown) {
         search.charTyped(keyTyped, __unknown);
+        reloadBlocks();
 
         return super.charTyped(keyTyped, __unknown);
-    }
-
-    static class ScrollableList extends AbstractScrollingList {
-        ScrollableList(Minecraft client, int width, int height, int top, int bottom, int left, int entryHeight, List<BlockItem> blocks) {
-            super(client, width, height, top, bottom, left, entryHeight);
-
-            blocks.forEach(e -> {
-                if( e == null )
-                    return;
-
-                addEntry(new Entry(e));
-            });
-        }
-
-        @Override
-        protected boolean isSelected(int index) {
-            return false;
-        }
-
-        private static class Entry extends AbstractScrollingList.AbstractListEntry {
-            BlockItem data;
-
-            public Entry(BlockItem data) {
-                this.data = data;
-            }
-
-            @Override
-            public void render(int entryIdx, int top, int left, int entryWidth, int entryHeight, int mouseX, int mouseY, boolean p_194999_5_, float partialTicks) {
-                FontRenderer font = Minecraft.getInstance().fontRenderer;
-
-                ResourceLocation domain = data.getItemStack().getItem().getRegistryName();
-                if( domain == null )
-                    domain = new ResourceLocation("");
-
-                font.drawString(data.getItemStack().getDisplayName().getFormattedText(), left + 30, top + 7, 0xFFFFFF);
-                font.drawString(domain.getNamespace(), left + 30, top + 17, 0xFFFFFF);
-
-                RenderHelper.enableGUIStandardItemLighting();
-                Minecraft.getInstance().getItemRenderer().renderItemAndEffectIntoGUI(data.getItemStack(), left + 5, top + 7);
-                RenderHelper.disableStandardItemLighting();
-            }
-        }
     }
 
     @Override
@@ -107,27 +58,28 @@ public class GuiBlockList extends GuiBase {
     }
 
     private void reloadBlocks() {
-        blocks = new ArrayList<>();
-        ArrayList<BlockItem> tmpBlocks = new ArrayList<>();
-        for( BlockItem block : XRay.gameBlockStore.getStore() ) {
-            if( block.getItemStack().getDisplayName().getFormattedText().toLowerCase().contains(search.getText().toLowerCase()) )
-                tmpBlocks.add(block);
-        }
-        blocks = tmpBlocks;
-//        this.blockList.updateBlockList( blocks );
+        if (this.lastSearched.equals(search.getText()) || search.getText().length() < 2)
+            return;
+
+        this.blockList.updateEntries(
+                this.blocks.stream().filter(e ->
+                        e.getItemStack().getDisplayName().getFormattedText().toLowerCase().contains(search.getText().toLowerCase())
+                ).collect(Collectors.toList())
+        );
+
         lastSearched = search.getText();
+        this.blockList.setScrollAmount(0);
     }
 
     @Override
-    public void render( int x, int y, float f )
-    {
+    public void render(int x, int y, float f) {
         super.render(x, y, f);
         search.render(x, y, f);
         blockList.render(x, y, f);
     }
 
     @Override
-    public boolean mouseClicked( double x, double y, int button ) {
+    public boolean mouseClicked(double x, double y, int button) {
         search.mouseClicked(x, y, button);
         return super.mouseClicked(x, y, button);
     }
@@ -135,21 +87,62 @@ public class GuiBlockList extends GuiBase {
     @Override
     public boolean mouseScrolled(double p_mouseScrolled_1_, double p_mouseScrolled_3_, double p_mouseScrolled_5_) {
         blockList.mouseScrolled(p_mouseScrolled_1_, p_mouseScrolled_3_, p_mouseScrolled_5_);
-        return super.mouseScrolled(p_mouseScrolled_1_ , p_mouseScrolled_3_ , p_mouseScrolled_5_);
+        return super.mouseScrolled(p_mouseScrolled_1_, p_mouseScrolled_3_, p_mouseScrolled_5_);
     }
 
-    @Override
-    public boolean mouseDragged(double p_mouseDragged_1_, double p_mouseDragged_3_, int p_mouseDragged_5_, double p_mouseDragged_6_, double p_mouseDragged_8_) {
-        blockList.mouseDragged(p_mouseDragged_1_, p_mouseDragged_3_, p_mouseDragged_5_, p_mouseDragged_6_, p_mouseDragged_8_);
-        return false;
-    }
+    static class ScrollingBlockList extends ScrollingList<ScrollingBlockList.BlockSlot> {
+        static final int SLOT_HEIGHT = 35;
 
-    //    @Override
-//    public void handleMouseInput() throws IOException {
-//        super.handleMouseInput();
-//
-//        int mouseX = Mouse.getEventX() * this.width / this.mc.displayWidth;
-//        int mouseY = this.height - Mouse.getEventY() * this.height / this.mc.displayHeight - 1;
-//        this.blockList.handleMouseInput(mouseX, mouseY);
-//    }
+        ScrollingBlockList(int x, int y, int width, int height, List<BlockItem> blocks) {
+            super(x, y, width, height, SLOT_HEIGHT);
+            this.updateEntries(blocks);
+        }
+
+        @Override
+        public void setSelected(@Nullable BlockSlot entry) {
+            if (entry == null)
+                return;
+
+            Minecraft.getInstance().player.closeScreen();
+            Minecraft.getInstance().displayGuiScreen(new GuiAddBlock(entry.getBlock(), null));
+        }
+
+        void updateEntries(List<BlockItem> blocks) {
+            this.clearEntries();
+            blocks.forEach(block -> this.addEntry(new BlockSlot(block, this)));
+        }
+
+        public static class BlockSlot extends AbstractList.AbstractListEntry<ScrollingBlockList.BlockSlot> {
+            BlockItem block;
+            ScrollingBlockList parent;
+
+            BlockSlot(BlockItem block, ScrollingBlockList parent) {
+                this.block = block;
+                this.parent = parent;
+            }
+
+            public BlockItem getBlock() {
+                return block;
+            }
+
+            @Override
+            public void render(int entryIdx, int top, int left, int entryWidth, int entryHeight, int mouseX, int mouseY, boolean p_194999_5_, float partialTicks) {
+                FontRenderer font = this.parent.minecraft.fontRenderer;
+
+                ResourceLocation resource = this.block.getItemStack().getItem().getRegistryName();
+                font.drawString(this.block.getItemStack().getItem().getName().getFormattedText(), left + 30, top + 7, Color.WHITE.getRGB());
+                font.drawString(resource != null ? resource.getNamespace() : "", left + 30, top + 17, Color.WHITE.getRGB());
+
+                RenderHelper.enableGUIStandardItemLighting();
+                this.parent.minecraft.getItemRenderer().renderItemAndEffectIntoGUI(this.block.getItemStack(), left + 5, top + 7);
+                RenderHelper.disableStandardItemLighting();
+            }
+
+            @Override
+            public boolean mouseClicked(double p_mouseClicked_1_, double p_mouseClicked_3_, int p_mouseClicked_5_) {
+                this.parent.setSelected(this);
+                return false;
+            }
+        }
+    }
 }
---
+++ src/main/java/com/xray/gui/utils/ScrollingList.java
@@ -0,0 +1,44 @@
+package com.xray.gui.utils;
+
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.widget.list.AbstractList;
+import org.lwjgl.opengl.GL11;
+
+/**
+ * A bare bones implementation of the {@link AbstractList} / {@link net.minecraft.client.gui.widget.list.ExtendedList}
+ * without the background or borders. With GL_SCISSOR to crop out the overflow
+ *
+ * This is how an abstract implementation should look... :cry:
+ */
+public class ScrollingList<E extends AbstractList.AbstractListEntry<E>> extends AbstractList<E> {
+    public ScrollingList(int x, int y, int width, int height, int slotHeightIn) {
+        super(Minecraft.getInstance(), width, height, y - (height / 2), (y - (height / 2)) + height, slotHeightIn);
+        this.setLeftPos(x - (width / 2));
+    }
+
+    @Override
+    public void render(int mouseX, int mouseY, float partialTicks) {
+        double scale = Minecraft.getInstance().mainWindow.getGuiScaleFactor();
+
+        GL11.glEnable(GL11.GL_SCISSOR_TEST);
+        GL11.glScissor((int) (this.x0 * scale), (int) (Minecraft.getInstance().mainWindow.getHeight() - ((this.y0 + height) * scale)),
+                (int) (width * scale), (int) (height * scale));
+
+        super.render(mouseX, mouseY, partialTicks);
+
+        GL11.glDisable(GL11.GL_SCISSOR_TEST);
+    }
+
+    @Override
+    protected int getScrollbarPosition() {
+        return (this.x0 + this.width) - 6;
+    }
+
+    @Override
+    public int getRowWidth() {
+        return this.width;
+    }
+
+    @Override
+    protected void renderHoleBackground(int p_renderHoleBackground_1_, int p_renderHoleBackground_2_, int p_renderHoleBackground_3_, int p_renderHoleBackground_4_) { }
+}
