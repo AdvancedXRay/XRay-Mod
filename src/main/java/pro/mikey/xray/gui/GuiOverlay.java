ee8b99473787b1ef5f0becac67c3b54c1bd9eb82
Michael Hillcox <michaelhillcox@gmail.com>
2024-05-09 00:13:09 +0100

    feat: start of port but stuck due to
    https://github.com/MinecraftForge/MinecraftForge/blob/884829421008a34b4721448747ccba5b9d5c7025/src/main/java/net/minecraftforge/client/event/RenderGuiOverlayEvent.java#L27

--- src/main/java/pro/mikey/xray/gui/GuiOverlay.java
+++ src/main/java/pro/mikey/xray/gui/GuiOverlay.java
@@ -7,7 +7,6 @@ import net.minecraft.client.resources.language.I18n;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.api.distmarker.OnlyIn;
-import net.minecraftforge.client.event.RenderGuiOverlayEvent;
 import net.minecraftforge.eventbus.api.EventPriority;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
