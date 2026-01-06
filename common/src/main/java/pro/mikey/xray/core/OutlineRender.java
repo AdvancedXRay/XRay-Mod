3c6cc39f9e7169a8e01242d010cfd56b5e10f143
laolitou2004 <laolitou@gmail.com>
2026-01-06 19:35:34 +0800

    Changes to be committed: 	modified:   common/src/main/java/pro/mikey/xray/core/ChunkScanTask.java 	modified:   common/src/main/java/pro/mikey/xray/core/OutlineRender.java 	modified:   common/src/main/java/pro/mikey/xray/core/ScanController.java 	modified:   common/src/main/java/pro/mikey/xray/core/scanner/BlockScanType.java 	modified:   common/src/main/java/pro/mikey/xray/core/scanner/ScanStore.java 	modified:   common/src/main/java/pro/mikey/xray/core/scanner/ScanType.java 	modified:   common/src/main/java/pro/mikey/xray/screens/ScanManageScreen.java

--- common/src/main/java/pro/mikey/xray/core/OutlineRender.java
+++ common/src/main/java/pro/mikey/xray/core/OutlineRender.java
@@ -28,8 +28,10 @@ import java.util.*;
 public class OutlineRender {
 	private static final RenderSystem.AutoStorageIndexBuffer indices = RenderSystem.getSequentialBuffer(VertexFormat.Mode.LINES);
 	private static final Map<ChunkPos, VBOHolder> vertexBuffers = new HashMap<>();
+	private static final Map<ChunkPos, VBOHolder> vertexBuffersa = new HashMap<>();
 
 	private static final Set<ChunkPos> chunksToRefresh = Collections.synchronizedSet(new HashSet<>());
+	private static final Set<ChunkPos> chunksToRefresha = Collections.synchronizedSet(new HashSet<>());
 
 	public static void renderBlocks(PoseStack poseStack) {
 		if (!ScanController.INSTANCE.isXRayActive() || Minecraft.getInstance().player == null) {
@@ -37,92 +39,187 @@ public class OutlineRender {
 		}
 
 		if (ScanController.INSTANCE.syncRenderList.isEmpty()) {
-			return;
-		}
-
-		if (!chunksToRefresh.isEmpty()) {
-			// Clear the vertex buffers for the chunks that need to be refreshed
-			for (ChunkPos pos : chunksToRefresh) {
-				VBOHolder holder = vertexBuffers.remove(pos);
-				if (holder != null) {
-					holder.close();
-				}
-			}
-
-			chunksToRefresh.clear();
-		}
-
-		// Clone the entrySet to avoid concurrent modification exceptions
-		var entries = new ArrayList<>(ScanController.INSTANCE.syncRenderList.entrySet());
-
-		for (var chunkWithBlockData : entries) {
-			var chunkPos = chunkWithBlockData.getKey();
-			var blocksWithProps = chunkWithBlockData.getValue();
-
-			if (blocksWithProps.isEmpty()) {
-				continue;
-			}
-
-			VBOHolder holder = vertexBuffers.get(chunkPos);
-			if (holder == null) {
-				BufferBuilder bufferBuilder = Tesselator.getInstance().begin(RenderPipelines.LINES.getVertexFormatMode(), RenderPipelines.LINES.getVertexFormat());
-
-				// More concurrent modification exceptions can happen here, so we clone the list
-				var blockPropsClone = new ArrayList<>(blocksWithProps);
-
-				for (var blockProps : blockPropsClone) {
-					if (blockProps == null) {
-						continue;
+			//return;
+		} else {
+			if (!chunksToRefresh.isEmpty()) {
+				// Clear the vertex buffers for the chunks that need to be refreshed
+				for (ChunkPos pos : chunksToRefresh) {
+					VBOHolder holder = vertexBuffers.remove(pos);
+					if (holder != null) {
+						holder.close();
 					}
-
-                    final int x = blockProps.x(), y = blockProps.y(), z = blockProps.z();
-
-					ShapeRenderer.renderShape(poseStack, bufferBuilder, Shapes.block(), x, y, z, blockProps.color(), 1f);
 				}
-
-				try (MeshData meshData = bufferBuilder.buildOrThrow()) {
-					int indexCount = meshData.drawState().indexCount();
-					GpuBuffer vertexBuffer = RenderSystem.getDevice()
-							.createBuffer(() -> "Xray vertex buffer", GpuBuffer.USAGE_VERTEX, meshData.vertexBuffer());
-
-					vertexBuffers.put(chunkPos, new VBOHolder(vertexBuffer, indexCount));
+	
+				chunksToRefresh.clear();
+			}
+	
+			// Clone the entrySet to avoid concurrent modification exceptions
+			var entries = new ArrayList<>(ScanController.INSTANCE.syncRenderList.entrySet());
+	
+			for (var chunkWithBlockData : entries) {
+				var chunkPos = chunkWithBlockData.getKey();
+				var blocksWithProps = chunkWithBlockData.getValue();
+	
+				if (blocksWithProps.isEmpty()) {
+					continue;
 				}
+	
+				VBOHolder holder = vertexBuffers.get(chunkPos);
+				if (holder == null) {
+					BufferBuilder bufferBuilder = Tesselator.getInstance().begin(RenderPipelines.LINES.getVertexFormatMode(), RenderPipelines.LINES.getVertexFormat());
+	
+					// More concurrent modification exceptions can happen here, so we clone the list
+					var blockPropsClone = new ArrayList<>(blocksWithProps);
+	
+					for (var blockProps : blockPropsClone) {
+						if (blockProps == null) {
+							continue;
+						}
+	
+						final int x = blockProps.x(), y = blockProps.y(), z = blockProps.z();
+	
+						ShapeRenderer.renderShape(poseStack, bufferBuilder, Shapes.block(), x, y, z, blockProps.color(), 1f);
+					}
+	
+					try (MeshData meshData = bufferBuilder.buildOrThrow()) {
+						int indexCount = meshData.drawState().indexCount();
+						GpuBuffer vertexBuffer = RenderSystem.getDevice()
+								.createBuffer(() -> "Xray vertex buffer", GpuBuffer.USAGE_VERTEX, meshData.vertexBuffer());
+	
+						vertexBuffers.put(chunkPos, new VBOHolder(vertexBuffer, indexCount));
+					}
+				}
+	
+				holder = vertexBuffers.get(chunkPos);
+				if (holder == null || holder.vertexBuffer == null || holder.indexCount == 0) {
+					continue;
+				}
+	
+				Vec3 playerPos = Minecraft.getInstance().gameRenderer.getMainCamera().position().reverse();
+	
+				Matrix4fStack matrix4fStack = RenderSystem.getModelViewStack();
+				GpuTextureView colorTextureView = Minecraft.getInstance().getMainRenderTarget().getColorTextureView();
+				GpuTextureView depthTextureView = Minecraft.getInstance().getMainRenderTarget().getDepthTextureView();
+	
+				matrix4fStack.pushMatrix();
+				matrix4fStack.translate((float) playerPos.x(), (float) playerPos.y(), (float) playerPos.z());
+				GpuBufferSlice[] gpubufferslice = RenderSystem.getDynamicUniforms().writeTransforms(new DynamicUniforms.Transform(new Matrix4f(matrix4fStack), new Vector4f(1.0F, 1.0F, 1.0F, 1.0F), new Vector3f(), new Matrix4f()));
+	
+				GL11.glDisable(GL11.GL_DEPTH_TEST);
+				RenderSystem.setShaderFog(gpubufferslice[0]);
+	
+				GpuBuffer gpuBuffer = indices.getBuffer(holder.indexCount);
+				try (RenderPass renderPass = RenderSystem.getDevice()
+						.createCommandEncoder()
+						.createRenderPass(() -> "xray", colorTextureView, OptionalInt.empty(), depthTextureView, OptionalDouble.empty())) {
+	
+					RenderSystem.bindDefaultUniforms(renderPass);
+					renderPass.setVertexBuffer(0, holder.vertexBuffer);
+					renderPass.setIndexBuffer(gpuBuffer, indices.type());
+					renderPass.setUniform("DynamicTransforms", gpubufferslice[0]);
+					renderPass.setPipeline(RenderPipelines.LINES);
+					renderPass.drawIndexed(0, 0, holder.indexCount, 1);
+				}
+	
+				GL11.glEnable(GL11.GL_DEPTH_TEST);
+				matrix4fStack.popMatrix();
 			}
-
-			holder = vertexBuffers.get(chunkPos);
-			if (holder == null || holder.vertexBuffer == null || holder.indexCount == 0) {
-				continue;
-			}
-
-			Vec3 playerPos = Minecraft.getInstance().gameRenderer.getMainCamera().position().reverse();
-
-			Matrix4fStack matrix4fStack = RenderSystem.getModelViewStack();
-			GpuTextureView colorTextureView = Minecraft.getInstance().getMainRenderTarget().getColorTextureView();
-			GpuTextureView depthTextureView = Minecraft.getInstance().getMainRenderTarget().getDepthTextureView();
-
-			matrix4fStack.pushMatrix();
-			matrix4fStack.translate((float) playerPos.x(), (float) playerPos.y(), (float) playerPos.z());
-			GpuBufferSlice[] gpubufferslice = RenderSystem.getDynamicUniforms().writeTransforms(new DynamicUniforms.Transform(new Matrix4f(matrix4fStack), new Vector4f(1.0F, 1.0F, 1.0F, 1.0F), new Vector3f(), new Matrix4f()));
-
-            GL11.glDisable(GL11.GL_DEPTH_TEST);
-            RenderSystem.setShaderFog(gpubufferslice[0]);
-
-			GpuBuffer gpuBuffer = indices.getBuffer(holder.indexCount);
-			try (RenderPass renderPass = RenderSystem.getDevice()
-					.createCommandEncoder()
-					.createRenderPass(() -> "xray", colorTextureView, OptionalInt.empty(), depthTextureView, OptionalDouble.empty())) {
-
-				RenderSystem.bindDefaultUniforms(renderPass);
-				renderPass.setVertexBuffer(0, holder.vertexBuffer);
-				renderPass.setIndexBuffer(gpuBuffer, indices.type());
-				renderPass.setUniform("DynamicTransforms", gpubufferslice[0]);
-				renderPass.setPipeline(RenderPipelines.LINES);
-				renderPass.drawIndexed(0, 0, holder.indexCount, 1);
-			}
-
-            GL11.glEnable(GL11.GL_DEPTH_TEST);
-            matrix4fStack.popMatrix();
 		}
+
+		//
+		if (ScanController.INSTANCE.syncRenderLista.isEmpty()) {
+			return;
+		} else {
+			if (!chunksToRefresha.isEmpty()) {
+				// Clear the vertex buffers for the chunks that need to be refreshed
+				for (ChunkPos pos : chunksToRefresha) {
+					VBOHolder holder = vertexBuffersa.remove(pos);
+					if (holder != null) {
+						holder.close();
+					}
+				}
+	
+				chunksToRefresha.clear();
+			}
+	
+			// Clone the entrySet to avoid concurrent modification exceptions
+			var entries = new ArrayList<>(ScanController.INSTANCE.syncRenderLista.entrySet());
+	
+			for (var chunkWithBlockData : entries) {
+				var chunkPos = chunkWithBlockData.getKey();
+				var blocksWithProps = chunkWithBlockData.getValue();
+	
+				if (blocksWithProps.isEmpty()) {
+					continue;
+				}
+	
+				VBOHolder holder = vertexBuffersa.get(chunkPos);
+				if (holder == null) {
+					BufferBuilder bufferBuilder = Tesselator.getInstance().begin(RenderPipelines.LINES.getVertexFormatMode(), RenderPipelines.LINES.getVertexFormat());
+	
+					// More concurrent modification exceptions can happen here, so we clone the list
+					List<OutlineRenderTarget> blockPropsClone;
+					synchronized (blocksWithProps) {
+						blockPropsClone = new ArrayList<>(blocksWithProps);
+					}
+	
+					for (var blockProps : blockPropsClone) {
+						if (blockProps == null) {
+							continue;
+						}
+	
+						final int x = blockProps.x(), y = blockProps.y(), z = blockProps.z();
+	
+						ShapeRenderer.renderShape(poseStack, bufferBuilder, Shapes.block(), x, y, z, blockProps.color(), 1f);
+					}
+	
+					try (MeshData meshData = bufferBuilder.buildOrThrow()) {
+						int indexCount = meshData.drawState().indexCount();
+						GpuBuffer vertexBuffer = RenderSystem.getDevice()
+								.createBuffer(() -> "Xray vertex buffer", GpuBuffer.USAGE_VERTEX, meshData.vertexBuffer());
+	
+						vertexBuffersa.put(chunkPos, new VBOHolder(vertexBuffer, indexCount));
+					}
+				}
+	
+				holder = vertexBuffersa.get(chunkPos);
+				if (holder == null || holder.vertexBuffer == null || holder.indexCount == 0) {
+					continue;
+				}
+	
+				Vec3 playerPos = Minecraft.getInstance().gameRenderer.getMainCamera().position().reverse();
+	
+				Matrix4fStack matrix4fStack = RenderSystem.getModelViewStack();
+				GpuTextureView colorTextureView = Minecraft.getInstance().getMainRenderTarget().getColorTextureView();
+				GpuTextureView depthTextureView = Minecraft.getInstance().getMainRenderTarget().getDepthTextureView();
+	
+				matrix4fStack.pushMatrix();
+				matrix4fStack.translate((float) playerPos.x(), (float) playerPos.y(), (float) playerPos.z());
+				GpuBufferSlice[] gpubufferslice = RenderSystem.getDynamicUniforms().writeTransforms(new DynamicUniforms.Transform(new Matrix4f(matrix4fStack), new Vector4f(1.0F, 1.0F, 1.0F, 1.0F), new Vector3f(), new Matrix4f()));
+	
+				GL11.glDisable(GL11.GL_DEPTH_TEST);
+				RenderSystem.setShaderFog(gpubufferslice[0]);
+	
+				GpuBuffer gpuBuffer = indices.getBuffer(holder.indexCount);
+				try (RenderPass renderPass = RenderSystem.getDevice()
+						.createCommandEncoder()
+						.createRenderPass(() -> "xray", colorTextureView, OptionalInt.empty(), depthTextureView, OptionalDouble.empty())) {
+	
+					RenderSystem.bindDefaultUniforms(renderPass);
+					renderPass.setVertexBuffer(0, holder.vertexBuffer);
+					renderPass.setIndexBuffer(gpuBuffer, indices.type());
+					renderPass.setUniform("DynamicTransforms", gpubufferslice[0]);
+					renderPass.setPipeline(RenderPipelines.LINES);
+					renderPass.drawIndexed(0, 0, holder.indexCount, 1);
+				}
+	
+				GL11.glEnable(GL11.GL_DEPTH_TEST);
+				matrix4fStack.popMatrix();
+			}
+		}
+
+
+
 	}
 
 	public static void clearVBOs() {
@@ -132,6 +229,12 @@ public class OutlineRender {
 			}
 		}
 		vertexBuffers.clear();
+		for (VBOHolder holder : vertexBuffersa.values()) {
+			if (holder != null) {
+				holder.close();
+			}
+		}
+		vertexBuffersa.clear();
 	}
 
 	public static void clearVBOsFor(List<ChunkPos> removedChunks) {
@@ -144,6 +247,10 @@ public class OutlineRender {
 
 	public static void refreshVBOForChunk(ChunkPos pos) {
 		chunksToRefresh.add(pos);
+	}
+
+	public static void refreshVBOForChunka(ChunkPos pos) {
+		chunksToRefresha.add(pos);
 	}
 
 	private record VBOHolder(GpuBuffer vertexBuffer, int indexCount) implements Closeable {
